79d950195c7f1c8a782b175ecd923dfed320e3bd
Maciej Adamski <m.adamski@ideacode.pl>
2018-10-26 09:22:15 +0200

    Added sample Schedule Manager

---
+++ src/Model/ScheduleManager.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Adamski\Symfony\ScheduleBundle\Model;
+
+class ScheduleManager implements ManagerInterface {
+
+    /**
+     * {@inheritdoc}
+     */
+    public function schedule(Schedule $schedule) {
+        // TODO: ..
+    }
+}
